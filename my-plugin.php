5b7385bfc7ff39e332a394b55673895ded6fcf9d
hasan yousuf <hasany@gmail.com>
2017-04-09 05:13:31 +0000

    Added admin menu

--- my-plugin.php
+++ my-plugin.php
@@ -7,3 +7,19 @@ Author URI: cnn.com
 Description: description is here
 Version: 0.0.1
 */
+
+add_action("admin_menu","addMenu");
+function addMenu()
+{
+  add_menu_page("My Plugin","My Plugin Options",4,"my-plugin-options","myPluginMenu");
+  add_submenu_page("my-plugin-options", "Option 1", "Option 1", 4, "my-plugin-option-1","pluginoption1");
+}
+
+function myPluginMenu()
+{
+ echo "myPluginMenu";
+}
+function pluginoption1()
+{
+ echo "Option 1";
+}
