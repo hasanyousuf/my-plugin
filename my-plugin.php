2134b8c957ffc8f021ed11faf6be5a00cb6c6c3b
hasan yousuf <hasany@gmail.com>
2017-04-10 06:54:05 +0000

    Added order view

--- my-plugin.php
+++ my-plugin.php
@@ -11,15 +11,33 @@ Version: 0.0.1
 add_action("admin_menu","addMenu");
 function addMenu()
 {
-  add_menu_page("My Plugin","My Plugin Options",4,"my-plugin-options","myPluginMenu");
-  add_submenu_page("my-plugin-options", "Option 1", "Option 1", 4, "my-plugin-option-1","pluginoption1");
+  add_menu_page("First Plugin","Plugin Options",4,"my-plugin-options","myPluginMenu");
+  add_submenu_page("my-plugin-options", "Option 1", "Orders", 4, "orders","option1");
+  add_submenu_page("my-plugin-options", "Option 2", "Reports", 4, "my-plugin-option-2","option2");
+  add_submenu_page("my-plugin-options", "Option 3", "Settings", 4, "my-plugin-option-3","option3");
+  add_submenu_page("my-plugin-options", "Option 4", "System status", 4, "my-plugin-option-4","option4");
 }
 
 function myPluginMenu()
 {
  echo "myPluginMenu";
 }
-function pluginoption1()
+function option1()
 {
- echo "Option 1";
+ require_once("views/view-orders.php");
+ // echo "Option 1";
+}
+
+function option2()
+{
+
+}
+
+function option3()
+{
+}
+
+function option4()
+{
+ phpinfo();
 }
---
+++ views/view-orders.php
@@ -0,0 +1,19 @@
+<div class="wrap">
+<h1>My Plugin order form</h1>
+<form name=order id=order method=post>
+<table class="form-table">
+<tbody>
+<tr>
+<th>
+<label>Product title</label>
+</th>
+<td><input type=text name=product id=product>
+</td>
+</tr>
+<tr>
+<th></th><td><input type=submit></td>
+</tr>
+</tbody>
+</table>
+</form>
+</div>
